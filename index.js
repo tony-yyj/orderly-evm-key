33b41ba3331d93aeff920b0213beba6efff4f051
tony <tony@woo.network>
2023-12-18 18:15:54 +0800

    fix private key length

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 import crypto from 'crypto';
 
 import * as ed from '@noble/ed25519';
-import {decodeBase58, encodeBase58, solidityPackedKeccak256, AbiCoder, keccak256} from "ethers";
+import { decodeBase58, encodeBase58, solidityPackedKeccak256, AbiCoder, keccak256 } from "ethers";
 
 global.crypto = crypto;
 
-async function createKeyPair() {
-    const privKey = ed.utils.randomPrivateKey(); // Secure random private key
-    const pubKey = await ed.getPublicKeyAsync(privKey);
-    const orderlyKeyPair = {
-        privKey,
-        publicKey: `ed25519:${encodeBase58(pubKey)}`,
-        privateKey: encodeBase58(privKey),
-    };
-    return orderlyKeyPair;
+function retry(operation, maxRetries = 99){
+    return new Promise((resolve, reject) => {
+        let retries = 0;
+        const retryOperationWrapper = async () => {
+            try {
+                const result = await operation();
+                resolve(result);
+            } catch (error) {
+                retries++;
+                console.log('-- retries', retries);
+                if (retries < maxRetries) {
+                    retryOperationWrapper();
+                } else {
+                    reject(error);
+                }
+            }
+        };
+        retryOperationWrapper();
+    });
+}
+
+export async function generateOrderlyKeyPair() {
+    return new Promise((resolve, reject) => {
+        const privKey = ed.utils.randomPrivateKey(); // Secure random private key
+        const privateKey = encodeBase58(privKey);
+        if (privateKey.length !== 44) {
+            reject(new Error('key error'));
+        }
+
+        ed.getPublicKeyAsync(privKey).then((pubKey) => {
+            const orderlyKeyPair = {
+                publicKey: `ed25519:${encodeBase58(pubKey)}`,
+                privateKey: encodeBase58(privKey),
+            };
+            resolve(orderlyKeyPair);
+        });
+    });
+}
+
+export async function getNewOrderlyKeyPair() {
+    try {
+        return await retry(generateOrderlyKeyPair);
+    } catch (error) {
+        return error;
+    }
 }
 
 async function getPublicKey(privateKey) {
@@ -27,7 +63,7 @@ function calculateStringHash(input) {
     return solidityPackedKeccak256(['string'], [input]);
 }
 
-function getAccountId (userAddress, brokerId)  {
+function getAccountId(userAddress, brokerId) {
     const abicoder = AbiCoder.defaultAbiCoder();
     return keccak256(abicoder.encode(['address', 'bytes32'], [userAddress, calculateStringHash(brokerId)]));
 };
@@ -35,18 +71,19 @@ function getAccountId (userAddress, brokerId)  {
 function getAccountInfo(userAddress) {
 
     const accountId = getAccountId(userAddress, 'woofi_pro');
-    console.log('woofi_pro',{
-        
+    console.log('woofi_pro', {
+
         userAddress,
         accountId,
     })
 }
 
-async function init () {
+async function init() {
 
-    for (let i = 0; i < 10; i ++) {
-    const keyPair =         await createKeyPair();
-    console.log(keyPair);
+    for (let i = 0; i < 10; i++) {
+        const orderlyKeyPair = await getNewOrderlyKeyPair();
+
+        console.log(orderlyKeyPair);
     }
 
 
